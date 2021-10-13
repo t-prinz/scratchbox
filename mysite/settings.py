08ac772d0f32b74cb3bb16b8186cfba3e218ae3e
Trey Prinz <t.prinz.a@gmail.com>
2021-10-12 20:40:35 -0500

    added the View and rendering template

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,6 +15,13 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+from dotenv import load_dotenv
+
+project_folder = os.path.expanduser(BASE_DIR)
+load_dotenv(os.path.join(project_folder, '.env'))
+
+#load_dotenv(os.path.join(BASE_DIR, '.env'))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
